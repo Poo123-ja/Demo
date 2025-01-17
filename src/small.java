55b005004e293359b18c9607ac0ef3ad648eb8d2
pooja <pooja.gla_cs21@gla.ac.in>
2025-01-17 22:47:52 +0530

    smallest

---
+++ src/small.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class small {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        if(a<b){
+            System.out.println("a is smallest");
+        }
+        else if(a>b) {
+            System.out.println("b is smallest ");
+        }
+        else {
+            System.out.println("both are equal");
+        }
+
+    }
+}
